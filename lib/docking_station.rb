6e3972ff8a61e5f68361971ae869c7a71b4cf6f3
Lucy Stringer <lucyjstringer@gmail.com>
2020-10-29 10:02:55 +0000

    up to step 12 with AG before compare to walkthrough

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,22 @@
+require_relative "./bike"
+
 class DockingStation
+
+  attr_reader :bike_store
+
+  def initialize
+    @bike_store = []
+  end
+
   def release_bike
+    if @bike_store.empty?
+      raise StandardError.new "There were no bikes in the docking station"
+    else
+      Bike.new
+    end
+  end
+
+  def dock(bike)
+    @bike_store.push(bike)
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,26 @@
 require "docking_station"
+require "bike"
 
 describe DockingStation do
-  it { release_bike.is_expected.to eq "Enjoy your ride!" }
+  it { is_expected.to respond_to(:release_bike) }
+
+  it "creates a new, working, instance of bike if dock isn't empty" do
+    station = DockingStation.new
+    bike = Bike.new
+    station.dock(bike)
+    expect(station.release_bike.working?).to eq true
+  end
+
+  it { is_expected.to respond_to(:dock) }
+
+  it "index 0 in bike storage is not nil after bike being docked" do
+    station = DockingStation.new
+    bike = Bike.new
+    station.dock(bike)
+    expect(station.bike_store[0]).to_not eq nil
+  end
+
+  it "returns error if trying to release bike from empty docking station" do
+    expect{DockingStation.new.release_bike}.to raise_error
+  end
 end
