4794c07ad12cf63a7b8f5786c151a4cdca12fbae
Lucy Stringer <lucyjstringer@gmail.com>
2021-02-09 16:33:00 +0000

    added default capacity at initialization

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,8 +6,9 @@ class DockingStation
 
   DEFAULT_CAPACITY = 20
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @bikes = []
+    @capacity = capacity
   end
 
   def dock(bike)
@@ -23,7 +24,7 @@ class DockingStation
   private
 
   def full?
-    @bikes.count >= DEFAULT_CAPACITY
+    @bikes.count >= capacity
   end
 
   def empty?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -42,10 +42,29 @@ describe DockingStation do
     end
 
     it "raises an error when trying to dock a bike to full station" do
-      DockingStation::DEFAULT_CAPACITY.times do
-        subject.dock Bike.new
-      end
+      subject.capacity.times { subject.dock Bike.new }
       expect {subject.dock Bike.new }.to raise_error "There is already a bike docked"
     end
+  end
+
+  describe 'initialization' do
+    it 'has a variable capacity' do
+      docking_station = DockingStation.new(50)
+      50.times { docking_station.dock Bike.new }
+      expect{ docking_station.dock Bike.new }.to raise_error 'There is already a bike docked'
+    end
+
+    subject { DockingStation.new }
+    let(:bike) { Bike.new }
+    it 'defaults capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.dock(bike)
+      end
+    expect{ subject.dock(bike) }.to raise_error 'There is already a bike docked'
+  end
+  end
+
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
 end
